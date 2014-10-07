c5094515a6393e04245a897b4df232b7139c0809
Alessandro Gnoli <tebemis@gmail.com>
2014-10-07 18:41:10 -0500

    Sits there and waits for messages

--- src/main/java/org/github/nutella_framework/BasicBot.java
+++ src/main/java/org/github/nutella_framework/BasicBot.java
@@ -1,5 +1,7 @@
 package org.github.nutella_framework;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 /**
  * Created by tebemis on 10/7/14.
  */
@@ -8,6 +10,20 @@ public class BasicBot extends Bot {
     @Override
     public void run() {
         // The code for your bot goes here!
+        nutella.subscribe("test", new NutellaMessageListener() {
+            @Override
+            public void processMessage(JsonNode jsonNode) {
+                System.out.println("We just received a message via MQTT: " + jsonNode.toString() );
+            }
+        });
+
+        while(true) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // we have been interrupted
+            }
+        }
     }
 
 }
